f962c4658609435f60e028e9b57a6a0bc61b4440
Ian Platt <ianplatt@Ians-MacBook-Pro.local>
2023-05-20 22:01:16 -0400

    Add scaling

--- scripts/health_display.gd
+++ scripts/health_display.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+# Properties
+@export_category("Properties")
+@export var scale_modifier = 1
+
 # Nodes
 @onready
 var parent: BaseEntity = self.get_parent() as BaseEntity
@@ -9,11 +13,11 @@ var label: Label = $Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(parent)
-	print(parent.inverted)
+	self.scale *= scale_modifier
+	
 	if parent.inverted:
 		print("Inverting!")
-		self.scale = Vector2(-1, 1)
+		self.scale = Vector2(-scale_modifier, scale_modifier)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
