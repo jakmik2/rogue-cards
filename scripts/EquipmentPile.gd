504e58aa08bbea5d802a0c77f94909719bb4fa60
Ian Platt <ianplatt@Ians-MBP.lan>
2024-06-30 12:39:21 -0700

    Add random spawn location

--- scripts/EquipmentPile.gd
+++ scripts/EquipmentPile.gd
@@ -5,23 +5,27 @@ class_name EquipmentPile extends StaticBody2D
 
 @export var pile: Array[String] = []
 
+var rng = RandomNumberGenerator.new()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if Global.packed_equipment_pile != null:
 		var old_equipment_pile = Global.packed_equipment_pile
 		for code in old_equipment_pile:
 			add_equipment(code)
-			
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-	
+
 func add_equipment(code):
 	pile.push_front(code)
+	# Spawn it in a random position above the container
+	var rand_x = rng.randi_range(-70, 70)
+	var rand_y = rng.randi_range(-60, -120)
+	
 	var equipment_item: EquipmentItem = equipment_item_prefab.instantiate()
 	equipment_item.equipment_id = code
 	equipment_storage.add_child(equipment_item)
 	equipment_item.owner = equipment_storage
+	
+	equipment_item.position = Vector2(rand_x, rand_y)
 
 
 func _on_tree_exiting():
