21466cef485bd3cd01069d098f2b12cbd48f4f9f
jakmik2 <40475031+jakmik2@users.noreply.github.com>
2024-06-30 13:29:19 -0700

    Feat/equipment pile (#14)
    * Begin equipment pile
    
    * Add random spawn location
    
    * add rotation
    
    ---------
    
    Co-authored-by: Ian Platt <ianplatt@Ians-MBP.lan>

---
+++ scripts/EquipmentPile.gd
@@ -0,0 +1,35 @@
+class_name EquipmentPile extends StaticBody2D
+
+@onready var equipment_item_prefab = preload("res://scenes/EquipmentItem.tscn")
+@onready var equipment_storage = $EquipmentStorage
+
+@export var pile: Array[String] = []
+
+var rng = RandomNumberGenerator.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if Global.packed_equipment_pile != null:
+		var old_equipment_pile = Global.packed_equipment_pile
+		for code in old_equipment_pile:
+			add_equipment(code)
+
+func add_equipment(code):
+	pile.push_front(code)
+	# Spawn it in a random position above the container
+	var rand_x = rng.randi_range(-70, 70)
+	var rand_y = rng.randi_range(-60, -120)
+	var rand_rot = rng.randf_range(-PI/2, PI/2)
+	
+	var equipment_item: EquipmentItem = equipment_item_prefab.instantiate()
+	equipment_item.equipment_id = code
+	equipment_storage.add_child(equipment_item)
+	equipment_item.owner = equipment_storage
+	
+	equipment_item.rotate(rand_rot)
+	
+	equipment_item.position = Vector2(rand_x, rand_y)
+
+
+func _on_tree_exiting():
+	Global.packed_equipment_pile = pile
